ea9ad32662a4454673f94d6acedd39d0437ef4d9
Muhammad Luthfi <muhammadluthfi059@gmail.com>
2023-10-26 22:25:38 +0700

    fix: update

--- chan-concept-2/main.go
+++ chan-concept-2/main.go
@@ -12,60 +12,52 @@ func main() {
 	fmt.Printf("START - %v\n", start.Format(time.RFC3339Nano))
 	fmt.Println("------------------------------------------------------------------")
 
-	// init pool worker
-	initPool := 1000
-
-	// read csv or source data
-	messages := readMessages()
-
 	wg := sync.WaitGroup{}
 
 	chMessages := make(chan string)
 
-	for i := 0; i < initPool; i++ {
+	messages := readMessages()
+
+	poolSize := len(messages)
+
+	wg.Add(poolSize)
+
+	for i := 0; i < poolSize; i++ {
 		go worker(&wg, chMessages)
 	}
 
-	wg.Add(len(messages))
-	for _, msg := range messages {
-		chMessages <- msg
-	}
-
-	close(chMessages)
-
-	// resource limit :
-	// 100 milicore cpu
-	// 100 mb memory
-
-	// expectation:
-	// please using worker pool and the execution should n time faster than serial process
-
-	// if not using concurrency : 1s * 10000 = 10000s -> 2,7777777778h
-	fmt.Println("------------------------------------------------------------------")
-	end := time.Now()
-	fmt.Printf("END - %v. Time Since : %v\n", end.Format(time.RFC3339Nano), time.Since(start))
+	go func() {
+		for _, msg := range messages {
+			chMessages <- msg
+		}
+		close(chMessages)
+	}()
 
 	wg.Wait()
+
+	fmt.Println("------------------------------------------------------------------")
+	end := time.Now()
+	fmt.Printf("END - %v. Time Since: %v\n", end.Format(time.RFC3339Nano), time.Since(start))
 }
 
 func worker(wg *sync.WaitGroup, chMessage chan string) {
 	for msg := range chMessage {
-		defer wg.Done()
 		sendMessage(msg)
+		wg.Done()
 	}
 }
 
 func readMessages() []string {
 	messages := []string{}
-	max := 10000
+	max := 20
 	for i := 0; i < max; i++ {
-		messages = append(messages, strings.Replace("hi {:username}!", "{:username}", fmt.Sprintf("username%d", i), 1))
+		messages = append(messages, strings.Replace("hi {:username}!", "{:username}", fmt.Sprintf("username-%d", i), 1))
 	}
 	return messages
 }
 
 func sendMessage(message string) {
 	fmt.Printf("START - SEND MESSAGE '%v' at %v\n", message, time.Now().Format(time.RFC3339Nano))
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Second)
 	fmt.Printf("FINISH - SEND MESSAGE '%v' at %v\n", message, time.Now().Format(time.RFC3339Nano))
 }
